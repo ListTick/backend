863a8a1f46d3fe422f64e33e4e54535e69adeaab
przemyslawren <przemyslaw.ren@gmail.com>
2025-03-30 16:17:56 +0200

    fix: change mapping

--- src/main/java/com/pro/list_tick/account/controller/AccountController.java
+++ src/main/java/com/pro/list_tick/account/controller/AccountController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AccountController {
     private final AccountService accountService;
 
-    @RequestMapping("/create")
+    @PostMapping("/create")
     public ResponseEntity<String> createAccount(@Valid @RequestBody AccountDto accountDto) {
         accountService.createAccount(accountDto);
 
