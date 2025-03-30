a9809e77552fa70cf796b704188840591445037b
przemyslawren <przemyslaw.ren@gmail.com>
2025-03-30 16:18:53 +0200

    fix: annotation fix

--- src/main/java/com/pro/list_tick/account/controller/AccountController.java
+++ src/main/java/com/pro/list_tick/account/controller/AccountController.java
@@ -5,12 +5,12 @@ import com.pro.list_tick.account.service.AccountService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
 public class AccountController {
