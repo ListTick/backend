613c831f462f802b9924864a07790fc77b272cb1
przemyslawren <przemyslaw.ren@gmail.com>
2025-03-30 16:15:35 +0200

    feat: add event listeners, add event publication table

--- src/main/java/com/pro/list_tick/account/controller/AccountController.java
+++ src/main/java/com/pro/list_tick/account/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.pro.list_tick.account.controller;
 
 import com.pro.list_tick.account.dto.AccountDto;
 import com.pro.list_tick.account.service.AccountService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,7 +16,7 @@ public class AccountController {
     private final AccountService accountService;
 
     @RequestMapping("/create")
-    public ResponseEntity<String> createAccount(@RequestBody AccountDto accountDto) {
+    public ResponseEntity<String> createAccount(@Valid @RequestBody AccountDto accountDto) {
         accountService.createAccount(accountDto);
 
         return ResponseEntity.ok("Account created");
