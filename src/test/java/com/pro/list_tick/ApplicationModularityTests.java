8963efdbb60cd404baea4a34b48e450f648cf629
przemyslawren <przemyslaw.ren@gmail.com>
2025-04-01 23:13:08 +0200

    feat: isolate the test

--- src/test/java/com/pro/list_tick/ApplicationModularityTests.java
+++ src/test/java/com/pro/list_tick/ApplicationModularityTests.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 
 public class ApplicationModularityTests {
-    static ApplicationModules modules = ApplicationModules.of(ListTickApplication.class,
-            JavaClass.Predicates.resideInAPackage("com.pro.list_tick.event"));
 
     @Test
-    void bootstrapApplicationModules() {
+    void testModulesAreValid() {
+        ApplicationModules modules = ApplicationModules.of(ListTickApplication.class,
+                JavaClass.Predicates.resideInAPackage("com.pro.list_tick.event"));
+
         modules.verify();
     }
 }
