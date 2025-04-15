2d278427957abbd0bd1e688e96fa85570e86ccdb
przemyslawren <przemyslaw.ren@gmail.com>
2025-04-15 21:47:20 +0200

    feat: update tag dto validations

--- src/main/java/com/pro/list_tick/task/dto/TagRequestDto.java
+++ src/main/java/com/pro/list_tick/task/dto/TagRequestDto.java
@@ -1,11 +1,13 @@
 package com.pro.list_tick.task.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record TagRequestDto(
 
-        @NotBlank
+        @NotBlank(message = "Name cannot be blank")
+        @Max(value = 255, message = "Name should be less than 255 characters")
         String name,
 
         @Pattern(
