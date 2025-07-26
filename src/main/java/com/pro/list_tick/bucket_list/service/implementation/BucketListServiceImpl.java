69a795f88762cfa5ca4a34d8261b1e4ab4687cd3
mpankanin <pankanin.milosz@gmail.com>
2025-07-26 19:04:36 +0200

    add services

---
+++ src/main/java/com/pro/list_tick/bucket_list/service/implementation/BucketListServiceImpl.java
@@ -0,0 +1,195 @@
+package com.pro.list_tick.bucket_list.service.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+import com.pro.list_tick.bucket_list.repository.BucketListRepository;
+import com.pro.list_tick.bucket_list.service.CategoryService;
+import com.pro.list_tick.bucket_list.service.SharedBucketListService;
+import com.pro.list_tick.bucket_list.service.BucketListService;
+import com.pro.list_tick.shared.current_user.CurrentAccountService;
+import com.pro.list_tick.bucket_list.dto.AccountSharedWithRequestDto;
+import com.pro.list_tick.bucket_list.dto.AccountSharedWithResponseDto;
+import com.pro.list_tick.bucket_list.dto.BucketListRequestDTO;
+import com.pro.list_tick.bucket_list.dto.BucketListRequestUpdateDTO;
+import com.pro.list_tick.bucket_list.dto.BucketListResponseDTO;
+import com.pro.list_tick.bucket_list.exception.BucketListException;
+import com.pro.list_tick.bucket_list.mapper.BucketListMapper;
+import com.pro.list_tick.bucket_list.model.SharedBucketList;
+import com.pro.list_tick.bucket_list.model.BucketList;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class BucketListServiceImpl implements BucketListService {
+
+    private final BucketListRepository bucketListRepository;
+
+    private final CurrentAccountService currentAccountService;
+    private final CategoryService categoryService;
+    private final SharedBucketListService sharedBucketListService;
+
+    public BucketList getById(UUID id) {
+        log.debug("Getting the bucket list: {}", id);
+        final var bucketList = bucketListRepository.findById(id)
+            .orElseThrow(() -> new BucketListException(HttpStatus.NOT_FOUND, "Bucket list not found"));
+
+        final var accountId = currentAccountService.getCurrentAccountId();
+        if (!validateAccess(accountId, bucketList) &&
+            !validateSharedAccess(accountId, bucketList)) {
+            log.error("User doesn't have access to the bucket list: {}", bucketList.getId());
+            throw new BucketListException(HttpStatus.FORBIDDEN, "Access denied");
+        }
+        return bucketList;
+    }
+
+    public List<BucketListResponseDTO> getAllDTOByAccountId() {
+        final var accountId = currentAccountService.getCurrentAccountId();
+        log.debug("Getting all bucket lists for the accountId: {}", accountId);
+
+        var bucketLists = bucketListRepository.findAllActiveByAccountId(accountId);
+        var sharedBucketLists = sharedBucketListService.findAllActiveByAccountId(accountId);
+        List<BucketListResponseDTO> dtoList = new ArrayList<>(bucketLists.stream()
+            .map(list -> {
+                if (list.getShared()) {
+                    var sharedWithAccounts = getSharedWithAccounts(list);
+                    return BucketListMapper.toResponseDTO(list, sharedWithAccounts);
+                } else {
+                    return BucketListMapper.toResponseDTO(list);
+                }
+            })
+            .toList());
+        dtoList.addAll(sharedBucketLists.stream()
+            .map(SharedBucketList::getBucketList)
+            .map(list -> {
+                var sharedWithAccounts = getSharedWithAccounts(list);
+                return BucketListMapper.toResponseDTO(list, sharedWithAccounts);
+            })
+            .toList());
+        return dtoList;
+    }
+
+    @Transactional(transactionManager = "bucketListTransactionManager")
+    public BucketListResponseDTO create(BucketListRequestDTO bucketListRequestDTO) {
+        final var accountId = currentAccountService.getCurrentAccountId();
+        log.info("Creating a bucket list for the account id: {}, name: {}",
+            accountId, bucketListRequestDTO.name());
+
+        var bucketList = BucketListMapper.toModel(bucketListRequestDTO);
+        var category = categoryService.getById(bucketListRequestDTO.categoryId());
+
+        validateName(bucketListRequestDTO.name(), accountId);
+
+        bucketList.setAccountId(accountId);
+        bucketList.setCategory(category);
+        bucketList.setItems(new ArrayList<>());
+        bucketList.setSharedBucketLists(new ArrayList<>());
+        
+        var savedBucketList = bucketListRepository.save(bucketList);
+
+        if (bucketListRequestDTO.shared()) {
+            List<SharedBucketList> sharedLists = sharedBucketListService
+                .createSharedBucketLists(savedBucketList, bucketListRequestDTO.sharedWithAccounts());
+            savedBucketList.getSharedBucketLists().addAll(sharedLists);
+        }
+
+        log.info("Bucket list has been created: {}, accountId: {}, name: {}",
+                savedBucketList.getId(), savedBucketList.getAccountId(), savedBucketList.getName()
+        );
+
+        if (savedBucketList.getShared()) {
+            var sharedWithDto = getSharedWithAccounts(savedBucketList);
+            return BucketListMapper.toResponseDTO(savedBucketList, sharedWithDto);
+        }
+
+        return BucketListMapper.toResponseDTO(savedBucketList);
+    }
+
+    @Transactional(transactionManager = "bucketListTransactionManager")
+    public BucketListResponseDTO update(UUID id, BucketListRequestUpdateDTO bucketListRequestUpdateDTO) {
+        var accountId = currentAccountService.getCurrentAccountId();
+        log.info("Updating the bucket list: {}", id);
+        var bucketList = getById(id);
+
+        validateName(bucketListRequestUpdateDTO.name(), accountId);
+        bucketList.setName(bucketListRequestUpdateDTO.name());
+
+        bucketList.setActive(bucketListRequestUpdateDTO.active());
+        bucketList.setCategory(categoryService.getById(bucketListRequestUpdateDTO.categoryId()));
+        return BucketListMapper.toResponseDTO(bucketListRepository.save(bucketList));
+    }
+
+    private void validateName(String bucketListRequestUpdateDTO, UUID accountId) {
+        if (bucketListRepository.existsByNameAndAccountId(bucketListRequestUpdateDTO, accountId)) {
+            log.error("Bucket list name already exists: {}", bucketListRequestUpdateDTO);
+            throw new BucketListException(HttpStatus.CONFLICT, "Name already exists");
+        }
+    }
+
+    @Transactional(transactionManager = "bucketListTransactionManager")
+    public BucketListResponseDTO updateByFields(UUID id, BucketListRequestUpdateDTO bucketListRequestUpdateDTO) {
+        var accountId = currentAccountService.getCurrentAccountId();
+        log.info("Updating the bucket list by fields: {}", id);
+        var bucketList = getById(id);
+        if (Objects.nonNull(bucketListRequestUpdateDTO.name())) {
+            validateName(bucketListRequestUpdateDTO.name(), accountId);
+            bucketList.setName(bucketListRequestUpdateDTO.name());
+        }
+        if (Objects.nonNull(bucketListRequestUpdateDTO.active())) {
+            bucketList.setActive(bucketListRequestUpdateDTO.active());
+        }
+        if (Objects.nonNull(bucketListRequestUpdateDTO.categoryId())) {
+            var category = categoryService.getById(bucketListRequestUpdateDTO.categoryId());
+            bucketList.setCategory(category);
+        }
+
+        return BucketListMapper.toResponseDTO(bucketListRepository.save(bucketList));
+    }
+
+    public void delete(UUID id) {
+        log.info("Deleting the bucket list: {}", id);
+        final var bucketList = bucketListRepository.findById(id)
+            .orElseThrow(() -> new BucketListException(String.format("Bucket list not found: %s", id)));
+
+        var accountId = currentAccountService.getCurrentAccountId();
+        validateAccess(accountId, bucketList);
+        bucketListRepository.delete(bucketList);
+    }
+
+    public Boolean validateAccess(UUID accountId, BucketList bucketList) {
+        log.debug("Validating the bucket list access: {}", bucketList.getId());
+
+        return accountId.equals(bucketList.getAccountId());
+    }
+
+    public Boolean validateSharedAccess(UUID accountId, BucketList bucketList) {
+        log.debug("Validating the bucket list shared access");
+
+        var accountIds = sharedBucketListService.getAllAccountsByBucketListId(bucketList.getId());
+        return accountIds.stream().anyMatch(id -> id.equals(accountId));
+    }
+
+    private List<AccountSharedWithResponseDto> getSharedWithAccounts(BucketList bucketList) {
+        var accountId = currentAccountService.getCurrentAccountId();
+        return bucketList.getSharedBucketLists()
+            .stream()
+            .map(list -> {
+                if (!list.getAccountId().equals(accountId)) {
+                    var email = sharedBucketListService.getEmail(list.getAccountId());
+                    return new AccountSharedWithResponseDto(email);
+                } else {
+                    var email = sharedBucketListService.getEmail(bucketList.getAccountId());
+                    return new AccountSharedWithResponseDto(email);
+                }
+            })
+            .toList();
+    }
+
+}
---
+++ src/main/java/com/pro/list_tick/bucket_list/service/implementation/CategoryServiceImpl.java
@@ -0,0 +1,159 @@
+package com.pro.list_tick.bucket_list.service.implementation;
+
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+import com.pro.list_tick.bucket_list.repository.BLCategoryRepository;
+import com.pro.list_tick.bucket_list.service.CategoryService;
+import com.pro.list_tick.shared.api.AccountAPI;
+import com.pro.list_tick.shared.current_user.CurrentAccountService;
+import com.pro.list_tick.bucket_list.dto.CategoryRequestDTO;
+import com.pro.list_tick.bucket_list.dto.CategoryResponseDTO;
+import com.pro.list_tick.bucket_list.exception.CategoryException;
+import com.pro.list_tick.bucket_list.mapper.CategoryMapper;
+import com.pro.list_tick.bucket_list.model.Category;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class CategoryServiceImpl implements CategoryService {
+
+    private final AccountAPI accountAPI;
+    private final CurrentAccountService currentAccountService;
+
+    private final BLCategoryRepository categoryRepository;
+
+    private static final UUID SHARED_CATEGORY_ID = UUID.fromString("11111111-1111-1111-1111-111111111111");
+
+    public Category getById(UUID id) {
+        var accountId = currentAccountService.getCurrentAccountId();
+        log.debug("Getting a bucket list category by the accountId: {}", accountId);
+
+        var category = categoryRepository.findById(id)
+            .orElseThrow(() -> {
+                String errMessage = "Category not found";
+                log.error("{}: {}", errMessage, id);
+                return new CategoryException(HttpStatus.BAD_REQUEST, errMessage);
+            });
+        validateOwnership(category, accountId);
+        return category;
+    }
+
+    public List<CategoryResponseDTO> getAllDTOByAccountId() {
+        var accountId = currentAccountService.getCurrentAccountId();
+        log.debug("Getting all bucket list categories by the accountId: {}", accountId);
+
+        var categories = categoryRepository.findAllByAccountId(accountId);
+        categories.add(getSharedCategory());
+        return categories.stream()
+                .map(CategoryMapper::toResponseDTO)
+                .toList();
+    }
+
+    @Transactional(transactionManager = "bucketListTransactionManager")
+    public CategoryResponseDTO create(CategoryRequestDTO categoryRequestDTO) {
+        var accountId = currentAccountService.getCurrentAccountId();
+        log.debug("Creating a bucket list category for the accountId: {}, category name: {}",
+                accountId, categoryRequestDTO.name());
+
+        validateName(accountId, categoryRequestDTO.name());
+        validateColour(categoryRequestDTO);
+        var category = CategoryMapper.toModel(categoryRequestDTO);
+        category.setAccountId(accountId);
+        if (Objects.isNull(category.getColour())) {
+            category.setColour(accountAPI.getDefaultBucketListCategoryColour());
+        }
+
+        var savedCategory = categoryRepository.save(category);
+        log.info("The category has been created: {}", savedCategory.getId());
+        return CategoryMapper.toResponseDTO(savedCategory);
+    }
+
+    @Transactional(transactionManager = "bucketListTransactionManager")
+    public CategoryResponseDTO update(UUID id, CategoryRequestDTO categoryRequestDTO) {
+        var accountId = currentAccountService.getCurrentAccountId();
+        log.debug("Updating the category: {}, for the accountId: {}", id, accountId);
+
+        var category = getById(id);
+        validateName(accountId, categoryRequestDTO.name());
+        validateColour(categoryRequestDTO);
+
+        category.setName(categoryRequestDTO.name());
+        if (Objects.isNull(categoryRequestDTO.colour())) {
+            category.setColour(accountAPI.getDefaultBucketListCategoryColour());
+        } else {
+            category.setColour(categoryRequestDTO.colour());
+        }
+
+        var savedCategory = categoryRepository.save(category);
+        log.info("The category has been updated: {}", savedCategory.getId());
+        return CategoryMapper.toResponseDTO(savedCategory);
+    }
+
+    @Transactional(transactionManager = "bucketListTransactionManager")
+    public CategoryResponseDTO updateByFields(UUID id, CategoryRequestDTO categoryRequestDTO) {
+        var accountId = currentAccountService.getCurrentAccountId();
+        log.debug("Updating by fields the category: {}, for the accountId: {}", id, accountId);
+
+        var category = getById(id);
+        if (Objects.nonNull(categoryRequestDTO.name())) {
+            validateName(accountId, categoryRequestDTO.name());
+            category.setName(categoryRequestDTO.name());
+        }
+        if (Objects.nonNull(categoryRequestDTO.colour())) {
+            validateColour(categoryRequestDTO);
+            category.setColour(categoryRequestDTO.colour());
+        }
+
+        var savedCategory = categoryRepository.save(category);
+        log.info("The category has been updated by fields: {}", savedCategory.getId());
+        return CategoryMapper.toResponseDTO(savedCategory);
+    }
+
+    @Transactional(transactionManager = "bucketListTransactionManager")
+    public void delete(UUID id) {
+        log.debug("Deleting the bucket list category: {}", id);
+
+        var category = getById(id);
+
+        categoryRepository.delete(category);
+        log.info("Bucket list category has been deleted: {}", id);
+    }
+
+    public Category getSharedCategory() {
+        return getById(SHARED_CATEGORY_ID);
+    }
+
+    private void validateOwnership(Category category, UUID accountId) {
+        if (category.getId().equals(SHARED_CATEGORY_ID)) {
+            return;
+        }
+        if (!category.getAccountId().equals(accountId)) {
+            String errMessage = "Access denied";
+            log.error("{} {}: {}", errMessage, ",categoryId: ",category.getId());
+            throw new CategoryException(HttpStatus.CONFLICT, errMessage);
+        }
+    }
+
+    private void validateName(UUID accountId, String name) {
+        if (categoryRepository.existsByNameAndAccountId(name, accountId)) {
+            String errMessage = "Category name already exists";
+          log.error("{} :{}", errMessage, name);
+            throw new CategoryException(HttpStatus.CONFLICT, errMessage);
+        }
+    }
+
+    private void validateColour(CategoryRequestDTO categoryRequestDTO) {
+        if (Objects.nonNull(categoryRequestDTO.colour()) && categoryRequestDTO.colour().length() != 7) {
+            throw new CategoryException("Category colour, if specified, must be exactly 7 characters long");
+        }
+    }
+
+}
