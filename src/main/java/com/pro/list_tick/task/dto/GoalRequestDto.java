2b6c1a5aa9b34fcc07ffc34f35f0f17d01e369b5
przemyslawren <przemyslaw.ren@gmail.com>
2025-08-25 21:27:49 +0200

    feat: add validation to goal priority

--- src/main/java/com/pro/list_tick/task/dto/GoalRequestDto.java
+++ src/main/java/com/pro/list_tick/task/dto/GoalRequestDto.java
@@ -1,8 +1,6 @@
 package com.pro.list_tick.task.dto;
 
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
@@ -13,6 +11,9 @@ public record GoalRequestDto(
 
         @Size(max = 512, message = "Description should be at most 512 characters")
         String description,
+
+        @Min(value = 1, message = "Priority should be at least 1")
+        @Max(value = 6, message = "Priority should be at most 6")
         Integer priority,
 
         @FutureOrPresent(message = "Start date cannot be in the past")
